f01457ffb938d4d91942da4e1f7f16e84e70b159
Parambir Singh <paramstr@gmail.com>
2023-01-19 23:38:14 +1300

    Fixed Tailwind configs and its all ready now

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ], //tells tailwind to direct its attention
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
