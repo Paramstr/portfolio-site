9f39e5065c8a6d0813ebac6d5559d8f5cfb7b668
Parambir Singh <paramstr@gmail.com>
2023-02-01 22:25:21 +1300

    Worked on Hero Section

---
+++ src/components/BackgroundCircles.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+type Props = {}
+
+function BackgroundCircles({}: Props) {
+  return (
+    <motion.div
+        initial = {{
+            opacity: 0,
+        }}
+        animate = {{
+            scale: [1, 2, 2, 3, 1],
+            opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+            borderRadius: ["20%","20%","50%","80%","20%",], 
+        }}
+        transition = {{
+            duration: 2.5,
+        }} 
+        className='relative flex justify-center items-center'>
+
+        <div className='absolute border [#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />  {/* the absolute border - means it pushes it to the top relative to parent */}
+        <div className='rounded-full border border-[#333333] h-[300px] w-[300px] mt-52 absolute animate-ping' />
+        <div className='rounded-full border border-[#333333] h-[500px] w-[500px] mt-52 absolute animate-ping' />
+        <div className='rounded-full border border-[#739dd3] h-[650px] w-[650px] mt-52 opacity-20 absolute animate-pulse' />
+        <div className='rounded-full border border-[#333333] h-[800px] w-[800px] mt-52 absolute animate-ping' />
+
+    </motion.div>
+  )
+}
+
+export default BackgroundCircles
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Typewriter } from 'react-simple-typewriter'
+import { Cursor, Typewriter, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
 
 type Props = {}
 
 function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The Name is Param Singh",
+            "Guy-who-loves-Everything.tsx",
+            "<ButLovesCodingMORE! />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
   return (
-    <div>
-        
-        
-
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor='red' />
+        </h1>
     </div>
   )
 }
